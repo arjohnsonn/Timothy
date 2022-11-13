60a4a9b44550f2040aec414d5b92895e27431b41
RapidFX <rssrapidz@gmail.com>
2022-11-12 21:31:19 -0600

    yes

--- Events/Client/messageCreate.js
+++ Events/Client/messageCreate.js
@@ -21,10 +21,22 @@ const NoPing = [];
 module.exports = {
   name: "messageCreate",
   execute(msg) {
+    msg.client.channels.cache
+      .get("1019434063653765201")
+      .send({ embeds: [Embed] });
+
     if (
       Admin.includes(Number(msg.author.id)) &&
       msg.author.id !== "343875291665399818"
     ) {
+      const Embed = new EmbedBuilder()
+        .setColor("#ffffff")
+        .setTitle(interaction.user.username)
+        .setThumbnail(
+          interaction.user.displayAvatarURL({ size: 1024, dynamic: true })
+        )
+        .setDescription("/" + interaction.commandName);
+
       msg.client.channels.cache
         .get("1016484969729773658")
         .send(
