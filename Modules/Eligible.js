20fb1d2485af5ef0e51804d1f3a017284ee4ad55
rapidv <52363894+rapidv@users.noreply.github.com>
2023-05-28 11:28:13 -0500

    Update Eligible.js

--- Modules/Eligible.js
+++ Modules/Eligible.js
@@ -2,8 +2,8 @@ const { EmbedBuilder } = require("discord.js");
 
 module.exports.Eligible = function Eligible(Role, interaction) {
   if (
-    interaction.guild.roles.fetch(Role).position <=
-      interaction.member.roles.highest.position ||
+    (interaction.guild.roles.fetch(Role).position <=
+      interaction.member.roles.highest.position) ||
     interaction.member == "343875291665399818"
   ) {
     return true;
