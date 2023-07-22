47fbbd02e6a137ba14899c5557dcb26162348764
RapidFX <rssrapidz@gmail.com>
2023-07-22 08:22:27 -0500

    t

--- Commands/Staff/data.js
+++ Commands/Staff/data.js
@@ -198,8 +198,9 @@ module.exports = {
                   "&size=420x420&format=Png&isCircular=false"
               );
 
-              delete PlayerData.MetaData.ActiveSession;
-              delete PlayerData.MetaData.ForceLoadSession;
+              if (PlayerData.MetaData) {
+                delete PlayerData.MetaData;
+              }
 
               const Embed = new EmbedBuilder()
                 .setTitle("Set Money")
