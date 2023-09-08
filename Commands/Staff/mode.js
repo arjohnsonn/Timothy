8f297dd4e102e54daa17e9cd1751568b6c653f3a
RapidFX <rssrapidz@gmail.com>
2023-09-07 19:47:52 -0500

    Refresh MRA

--- Commands/Staff/mode.js
+++ Commands/Staff/mode.js
@@ -27,6 +27,7 @@ const data = require("./data");
 const globalannouncement = require("./globalannouncement");
 const kick = require("./kick");
 const unban = require("./unban");
+const MessageReactionAdd = require("../../Events/Client/MessageReactionAdd");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -91,6 +92,8 @@ module.exports = {
     globalannouncement.Refresh();
     kick.Refresh();
     unban.Refresh();
+
+    MessageReactionAdd.Refresh();
 
     setTimeout(() => {}, 500);
 
