9a17e858a891311ab9b3322b9990c0125a87615f
RapidFX <rssrapidz@gmail.com>
2023-11-21 19:44:00 -0600

    f

--- Commands/Staff/data.js
+++ Commands/Staff/data.js
@@ -29,9 +29,9 @@ function formatTime(seconds) {
   const m = Math.floor((seconds % 3600) / 60);
   const s = Math.round(seconds % 60);
 
-  if (!(h == 0)) {
+  if (!(h <= 0)) {
     return `${h}h ${m}m ${s}s`;
-  } else if (!(m == 0)) {
+  } else if (!(m <= 0)) {
     return `${m}m ${s}s`;
   } else {
     return `${s}s`;
