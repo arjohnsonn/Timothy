5794f91d881c99ebeb36ca95ddadeab811a5a3ee
RapidFX <rssrapidz@gmail.com>
2023-12-01 20:48:32 -0600

    For Fix

--- Modules/SetPlayerData.js
+++ Modules/SetPlayerData.js
@@ -1,11 +1,14 @@
 var { PlayerDataStore } = require("./DataStores");
 
 module.exports.SetPlayerData = function SetPlayerData(UserId, Data) {
-  for (const [key, value] of Data.Data.Logs.Chat) {
-    if (value.Chat.contains("#")) {
-      delete Data.Logs.Chat[key];
+  if (Data && Data.Data && Data.Data.Logs && Data.Data.logs.Chat) {
+    for (const [key, value] of Data.Data.Logs.Chat) {
+      if (value.Chat.contains("#")) {
+        delete Data.Logs.Chat[key];
+      }
     }
   }
+
   PlayerDataStore.SetAsync(UserId.toString(), Data)
     .then((Result) => {
       return true;
